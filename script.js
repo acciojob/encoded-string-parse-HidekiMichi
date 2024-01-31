c10e8455bcccf0e6d09f86a014ebce2ef3a464a0
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-31 14:40:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const parseCode = (str) => {
   // your code here
+	let s="";
+	let strArr=[];
+	let i=0;
+	let flag=true;
+	while(i<str.length){
+		if(str[i]=='0' && flag==true){
+			flag=false;
+			strArr.push(s);
+			s="";
+		}else if(str[i]!='0'){
+			s+=str[i];
+			flag=true;
+		}
+		i++;
+	}
+	strArr.push(s);
+	return {firstName:strArr[0],lastName:strArr[1],id:strArr[2]};
+	
 };
 
 // Do not change the code below
